d4eefa4469500addaf8f173859de3605eb8b18ec
Sherif Ashraf <sherif.ashraf.farooq@gmail.com>
2026-03-08 22:00:40 +0200

    feat: add Navigation Drawer and Toast events to WeatherEvent sealed class

--- app/src/main/java/com/example/weatherapp/presentation/WeatherEvent.kt
+++ app/src/main/java/com/example/weatherapp/presentation/WeatherEvent.kt
@@ -2,11 +2,11 @@ package com.example.weatherapp.presentation
 
 sealed class WeatherEvent {
 
+    object OpenNavigationDrawer : WeatherEvent()
     data class ScrollToPage(val pageIndex: Int) : WeatherEvent()
+    data class ShowToast(val message: String) : WeatherEvent()
 
     object GpsNotEnabled : WeatherEvent()
-
     object NetworkNotFound : WeatherEvent()
-
     object RequestLocationPermission : WeatherEvent()
 }
