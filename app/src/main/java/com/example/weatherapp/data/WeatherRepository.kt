2c9de2f40007e38013fdbeeffc30d4a05110a010
Sherif Ashraf <sherif.ashraf.farooq@gmail.com>
2026-03-09 00:34:29 +0200

    feat: link repository onboarding logic with preferenceManager setFirstRun

--- app/src/main/java/com/example/weatherapp/data/WeatherRepository.kt
+++ app/src/main/java/com/example/weatherapp/data/WeatherRepository.kt
@@ -2,12 +2,16 @@ package com.example.weatherapp.data
 
 import com.example.weatherapp.BuildConfig
 import com.example.weatherapp.R
+import com.example.weatherapp.data.datasource.local.PreferenceManager
 import com.example.weatherapp.data.datasource.remote.WeatherRemoteDataSource
 import com.example.weatherapp.data.models.current.CurrentWeatherResponse
 import com.example.weatherapp.data.models.forecast.ForecastResponse
 import com.example.weatherapp.data.models.home.FullWeatherData
 
-class WeatherRepository(val remoteDataSource: WeatherRemoteDataSource) {
+class WeatherRepository(
+    val remoteDataSource: WeatherRemoteDataSource,
+    private val preferenceManager: PreferenceManager
+) {
 
     suspend fun getHomeWeather(lat: Double, lon: Double, apiKey: String): Result<FullWeatherData> {
 
@@ -27,5 +31,14 @@ class WeatherRepository(val remoteDataSource: WeatherRemoteDataSource) {
         }
 
 
+    }
+
+    fun isFirstTimeUser(): Boolean {
+        return preferenceManager.isFirstRun()
+    }
+
+    fun saveInitialSetup(tempUnit: String, timeFormat: String, windUnit: String){
+        preferenceManager.saveSettings(tempUnit, timeFormat, windUnit)
+        preferenceManager.setFirstRun(false)
     }
 }
