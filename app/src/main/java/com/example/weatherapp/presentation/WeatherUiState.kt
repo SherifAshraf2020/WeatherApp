fa6c9469742e8e03f9dabf52bb7a005f697b6191
Sherif Ashraf <sherif.ashraf.farooq@gmail.com>
2026-03-08 19:53:38 +0200

    feat: add WeatherUiState for home screen

---
+++ app/src/main/java/com/example/weatherapp/presentation/WeatherUiState.kt
@@ -0,0 +1,9 @@
+package com.example.weatherapp.presentation
+
+import com.example.weatherapp.data.models.home.FullWeatherData
+
+sealed class WeatherUiState {
+    object Loading : WeatherUiState()
+    data class Success(val data: FullWeatherData) : WeatherUiState()
+    data class Error(val message: String) : WeatherUiState()
+}
